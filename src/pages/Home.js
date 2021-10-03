6b482a3ff3e11a0ebb36c82fecfbd8029727708c
Mustafa Akbaş <makbash21@gmail.com>
2021-10-03 18:13:21 +0300

    className update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div className="container mt-4">
-      <div class="jumbotron">
-        <h1 class="display-4">React-Django Integration</h1>
-        <p class="lead">
+      <div className="jumbotron">
+        <h1 className="display-4">React-Django Integration</h1>
+        <p className="lead">
           It is a demonstration of how to integrate react/contextAPI with
           django/django_rest_framework(djoser library) to build a fully functional web
           application.
         </p>
-        <p class="lead">
-          <Link class="btn btn-primary btn-lg" to="/login" role="button">
+        <p className="lead">
+          <Link className="btn btn-primary btn-lg" to="/login" role="button">
             Get Started
           </Link>
         </p>
